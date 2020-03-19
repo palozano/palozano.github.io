89b66daccb5a3b02b714e5a0d0cabf973105fec1
Pablo Lozano <pablo.lozano@uc3m.es>
2020-03-19 15:39:45 +0100

    Updates

---
+++ precache-manifest.4dabd3376eac9dc269f5d9e290b0c453.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fbb3c0bf9a994dc21768cbc5d8a6bf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9fcfd287ce744f3225a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4fa60a26138d03c62765",
+    "url": "/static/js/2.10ab848a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.10ab848a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9fcfd287ce744f3225a",
+    "url": "/static/js/main.bbb1c4ff.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "d912097c6baa28572cd00e0ed6ebddf5",
+    "url": "/static/media/profile.d912097c.jpeg"
+  }
+]);
