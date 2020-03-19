c0842962dba17884d1aa2160a790b22cb682de10
Pablo Lozano <pablo.lozano@uc3m.es>
2020-03-19 23:37:26 +0100

    Updates

--- precache-manifest.129fd0d6d75604a0b57dbfa56e70680f.js
+++ precache-manifest.129fd0d6d75604a0b57dbfa56e70680f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a66e81cdffdd2d5c545a105390a7fe8d",
+    "revision": "a1a04f9cd0d95a423a00cfb50624a0d2",
     "url": "/index.html"
   },
   {
-    "revision": "da43977f2a06cec2df4e",
+    "revision": "8db23e1a5f99d8f3a98c",
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "4fa60a26138d03c62765",
-    "url": "/static/js/2.10ab848a.chunk.js"
+    "revision": "86e12da6f0443e2fb6e6",
+    "url": "/static/js/2.3dce018d.chunk.js"
   },
   {
     "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
-    "url": "/static/js/2.10ab848a.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.3dce018d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "da43977f2a06cec2df4e",
-    "url": "/static/js/main.cd029b41.chunk.js"
+    "revision": "8db23e1a5f99d8f3a98c",
+    "url": "/static/js/main.4858194d.chunk.js"
   },
   {
     "revision": "0d46f7656c748826843c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.916ec46f17d62b1df93e7a25d3d0000b.js"
+  "/precache-manifest.129fd0d6d75604a0b57dbfa56e70680f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
